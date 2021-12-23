f8d2a83078d73108c2e2c99c3bf2ad431111eac1
axelr <axelr@LAPTOP-U6739NS7>
2021-12-23 18:46:57 +0100

    Ajoutez des fichiers projet.

---
+++ Game1.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+using MonoGame.Extended.Sprites;
+using MonoGame.Extended.Content;
+using MonoGame.Extended.Serialization;
+
+namespace TetraDungeon
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        // Steve
+        private Vector2 _stevePosition;
+        private AnimatedSprite _steve;
+        private float _vitesseSteve;
+        private string _animationSteve;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            _stevePosition = new Vector2(0, 0);
+            _vitesseSteve = 100;
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            SpriteSheet spriteSheet = Content.Load<SpriteSheet>("SteveSprite.sf", new JsonContentLoader());
+            _steve = new AnimatedSprite(spriteSheet);
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            float deltaSeconds = (float)gameTime.ElapsedGameTime.TotalSeconds;
+            float walkSpeed = deltaSeconds * _vitesseSteve;
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(_steve, _stevePosition);
+            _spriteBatch.End();
+            base.Draw(gameTime);
+        }
+    }
+}
