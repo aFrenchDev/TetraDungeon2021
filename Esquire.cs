ceb96dddc5102bc5b6e84d35942eb66790977f42
Axel Rouquette <axel.rouquette@etu.univ-smb.fr>
2022-01-03 11:22:42 +0100

    Message

---
+++ Esquire.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using MonoGame.Extended.Sprites;
+using MonoGame.Extended.Tiled;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TetraDungeon
+{
+    class Esquire
+    {
+
+        // Spawn
+        private Vector2 SpawnPosition;
+
+        // Infos
+        private Vector2 position;
+        private Vector2 direction;
+        private Vector2 destination;
+        private string _animation;
+        private AnimatedSprite _esquire;
+        private float _vitesseEsquire = 75;
+
+
+        public Esquire(AnimatedSprite _esquire, Vector2 spawnPosition, Vector2 destination)
+        {
+            this._esquire = _esquire;
+            this.SpawnPosition = spawnPosition;
+            this.Destination = destination;
+        }
+
+        public Vector2 Position
+        {
+            get
+            {
+                return this.Position;
+            }
+
+            set
+            {
+                this.Position = value;
+            }
+        }
+
+        public Vector2 Destination
+        {
+            get
+            {
+                return this.destination;
+            }
+
+            set
+            {
+                this.destination = value;
+            }
+        }
+
+        public void Behavior(Vector2 stevePosition, float deltaTime)
+        {
+            this.Position = new Vector2(160, 160);
+
+            float walkSpeed = deltaTime * _vitesseEsquire;
+            
+            this._esquire.Play("Idle");
+        }
+    }
+}
---
+++ Player.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Input;
+using MonoGame.Extended.Sprites;
+using MonoGame.Extended.Tiled;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TetraDungeon
+{
+    class Player
+    {
+        // Steve
+        private AnimatedSprite _steve;
+        private float vitesse = 75;
+        private float timerFootStep;
+        private int life;
+        private Vector2 position;
+
+        public Player(AnimatedSprite steve)
+        {
+            _steve = steve;
+        }
+
+        public Vector2 Position
+        {
+            get
+            {
+                return this.position;
+            }
+
+            set
+            {
+                this.position = value;
+            }
+        }
+
+        public int Life
+        {
+            get
+            {
+                return this.life;
+            }
+
+            set
+            {
+                this.life = value;
+            }
+        }
+
+        public void MovementSteve(float deltaSeconds, TiledMapTileLayer tiledMapTileLayer, TiledMap tiledMap, string animationSteve, ref Vector2 _stevePosition, ref string tempIdle, SoundEffect steveFootStep)
+        {
+            // GESTION DU CLAVIER
+            KeyboardState keyboardState = Keyboard.GetState();
+            float walkSpeed = deltaSeconds * vitesse;
+
+            animationSteve = tempIdle;
+
+            // -------------------------------------------------------
+            //                   DEPLACEMENT STEVE
+            // -------------------------------------------------------
+
+            if (keyboardState.IsKeyDown(Keys.Z))
+            {
+                ushort tx = (ushort)(_stevePosition.X / tiledMap.TileWidth);
+                ushort ty = (ushort)(_stevePosition.Y / tiledMap.TileHeight - 0.1);
+                if (!Collision.IsCollision(tx, ty, tiledMapTileLayer))
+                    _stevePosition.Y -= walkSpeed;
+
+                tempIdle = "idleBack";
+                animationSteve = "walkBack";
+            }
+            if (keyboardState.IsKeyDown(Keys.S))
+            {
+                ushort tx = (ushort)(_stevePosition.X / tiledMap.TileWidth);
+                ushort ty = (ushort)(_stevePosition.Y / tiledMap.TileHeight + 0.5);
+                if (!Collision.IsCollision(tx, ty, tiledMapTileLayer))
+                    _stevePosition.Y += walkSpeed;
+
+                tempIdle = "idleFront";
+                animationSteve = "walkFront";
+            }
+            if (keyboardState.IsKeyDown(Keys.Q))
+            {
+                ushort tx = (ushort)(_stevePosition.X / tiledMap.TileWidth - 0.5);
+                ushort ty = (ushort)(_stevePosition.Y / tiledMap.TileHeight);
+                if (!Collision.IsCollision(tx, ty, tiledMapTileLayer))
+                    _stevePosition.X -= walkSpeed;
+
+                tempIdle = "idleF";
+                animationSteve = "walkF";
+            }
+            if (keyboardState.IsKeyDown(Keys.D))
+            {
+                ushort tx = (ushort)((_stevePosition.X / tiledMap.TileWidth + 0.5));
+                ushort ty = (ushort)((_stevePosition.Y / tiledMap.TileHeight));
+                if (!Collision.IsCollision(tx, ty, tiledMapTileLayer))
+                    _stevePosition.X += walkSpeed;
+
+                tempIdle = "idle";
+                animationSteve = "walk";
+            }
+
+            if (animationSteve == "walkF" || animationSteve == "walk" || animationSteve == "walkFront" || animationSteve == "walkBack")
+            {
+                timerFootStep += deltaSeconds;
+                if (timerFootStep >= 0.35)
+                {
+                    timerFootStep = 0;
+                    steveFootStep.Play();
+                }
+            }
+
+            _steve.Play(animationSteve);
+        }
+    }
+}
